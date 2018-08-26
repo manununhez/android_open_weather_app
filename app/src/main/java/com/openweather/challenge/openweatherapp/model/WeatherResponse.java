2738cc52203a966e188cb37aa1e10266996c0859
mnunhez <03.manu@gmail.com>
2018-08-26 15:47:33 +0200

    Added control status network calls

--- app/src/main/java/com/openweather/challenge/openweatherapp/model/WeatherResponse.java
+++ app/src/main/java/com/openweather/challenge/openweatherapp/model/WeatherResponse.java
@@ -8,18 +8,18 @@ import java.util.Arrays;
  * Created by manuel on 23,August,2018
  */
 public class WeatherResponse {
-    private Coordinate coord;
-    private WeatherDescription[] weather;
-    private String base;
-    private Main main;
-    private int visibility;
-    private Wind wind;
-    private Clouds clouds;
-    private long dt;
-    private Sys sys;
-    private int id;
-    private String name;
-    private int cod;
+    public Coordinate coord;
+    public WeatherDescription[] weather;
+    public String base;
+    public Main main;
+    public int visibility;
+    public Wind wind;
+    public Clouds clouds;
+    public long dt;
+    public Sys sys;
+    public int id;
+    public String name;
+    public int cod;
 
 
     /**
